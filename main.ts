87684ac578cb960302fd751541b2a8ad271e3c3a
Jujhar Singh <1382005+jujhars13@users.noreply.github.com>
2025-11-10 19:34:46 +0000

    adds working code

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 let sonar = 0
-wuKong.setAllMotor(0, 0)
-basic.pause(1000)
+wuKong.setAllMotor(-100, -100)
 basic.forever(function () {
     sonar = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
     if (sonar < 20 && sonar > 1) {
-        wuKong.setAllMotor(-5, -5)
+        wuKong.setAllMotor(-50, 100)
         basic.pause(randint(0, 10))
     } else {
-        wuKong.setAllMotor(-5, -4)
+        wuKong.setAllMotor(-100, -100)
     }
 })
